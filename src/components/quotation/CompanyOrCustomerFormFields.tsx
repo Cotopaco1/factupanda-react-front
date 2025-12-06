aac8e5ee6edcf9ed5d7e0852a7b3fb2963f8f9ca
Sergio <cotopaco123@hotmail.es>
2025-12-05 23:48:10 -0500

    feat: create CompanyOrCustomerFormField component

---
+++ src/components/quotation/CompanyOrCustomerFormFields.tsx
@@ -0,0 +1,24 @@
+import type { Control } from "react-hook-form";
+import { FieldGroup } from "../ui/field";
+import { FormInput } from "../form/FormInput";
+
+interface Props {
+    suffix : string;
+    control : Control;
+}
+
+export function CompanyOrCustomerFormField({suffix, control}:Props){
+
+    return (
+        <FieldGroup className="grid md:grid-cols-2">
+            <FormInput name={`${suffix}.name`} control={control} label="Nombre" type="text"/>
+            <FormInput name={`${suffix}.address`} control={control} label="Dirección" type="text"/>
+            <FormInput name={`${suffix}.city`} control={control} label="Ciudad" type="text"/>
+            <FormInput name={`${suffix}.fiscal_number`} control={control} label="Numero Fiscal" type="text"/>
+            <FormInput name={`${suffix}.email`} control={control} label="Correo Electronico" type="email"/>
+            <FormInput name={`${suffix}.phone`} control={control} label="Correo Electronico" type="text"/>
+        </FieldGroup>
+    )
+
+
+}
