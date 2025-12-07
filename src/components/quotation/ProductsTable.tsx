28a038cf0e83b937245b385014bc8ad8131ec546
Sergio <cotopaco123@hotmail.es>
2025-12-07 17:07:57 -0500

    refactor: comment out unused TableFooter in ProductsTable component

--- src/components/quotation/ProductsTable.tsx
+++ src/components/quotation/ProductsTable.tsx
@@ -51,12 +51,12 @@ export function ProductsTable({products, onDelete}:Props) {
           </TableRow>
         ))}
       </TableBody>
-      <TableFooter>
+      {/* <TableFooter>
         <TableRow>
           <TableCell colSpan={3}>Total</TableCell>
           <TableCell className="text-right">$2,500.00</TableCell>
         </TableRow>
-      </TableFooter>
+      </TableFooter> */}
     </Table>
   )
 }
