d3a64f89fca6e8f0d3648c125de387d541b2fab6
Sergio <cotopaco123@hotmail.es>
2025-12-06 19:26:50 -0500

    fix: Label and required field in CompanyOrCustomerFormFields.

--- src/components/quotation/CompanyOrCustomerFormFields.tsx
+++ src/components/quotation/CompanyOrCustomerFormFields.tsx
@@ -11,12 +11,12 @@ export function CompanyOrCustomerFormField({suffix, control}:Props){
 
     return (
         <FieldGroup className="grid md:grid-cols-2">
-            <FormInput name={`${suffix}.name`} control={control} label="Nombre" type="text"/>
+            <FormInput name={`${suffix}.name`} control={control} label="Nombre" type="text" required/>
             <FormInput name={`${suffix}.address`} control={control} label="Dirección" type="text"/>
             <FormInput name={`${suffix}.city`} control={control} label="Ciudad" type="text"/>
             <FormInput name={`${suffix}.fiscal_number`} control={control} label="Numero Fiscal" type="text"/>
             <FormInput name={`${suffix}.email`} control={control} label="Correo Electronico" type="email"/>
-            <FormInput name={`${suffix}.phone`} control={control} label="Correo Electronico" type="text"/>
+            <FormInput name={`${suffix}.phone`} control={control} label="Telefono" type="text"/>
         </FieldGroup>
     )
 
