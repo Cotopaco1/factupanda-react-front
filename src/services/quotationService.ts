13168aa250fa40c67f30126b92ed13d09095d122
Sergio <cotopaco123@hotmail.es>
2025-12-06 19:25:31 -0500

    feat: update createQuotation function to specify response type and improve error logging

--- src/services/quotationService.ts
+++ src/services/quotationService.ts
@@ -2,14 +2,15 @@ import { apiClient } from "@/lib/apiClient";
 
 export const useQuotationService = () => {
 
-    const createQuotation = async (data: any) => {
+    const createQuotation = async (data: any) : Promise<Blob> => {
         try {
-            const response = await apiClient.post('/quotations', data);
+            const response = await apiClient.post('/quotations', data, { responseType : 'blob'});
+            console.log(response);
             return response.data;
         } catch (error: any) {
             // Agregar los errores del backend al objeto error para React Hook Form
             if (error.response?.data?.errors) {
-                console.log("Errores : ",error.response.data.errors);
+                console.log("Errores desde QuotationService : ",error.response.data.errors);
                 error.backendErrors = error.response.data.errors;
             }
             throw error;
