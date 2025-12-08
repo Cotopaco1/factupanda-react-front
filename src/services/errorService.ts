21df340aec691e440fef20dce7b003f34f91ce4d
Sergio <cotopaco123@hotmail.es>
2025-12-08 16:17:47 -0500

    feat: enhance error handling in MergeServerErrorsToForm and ApplyServerErrorsToRHF

--- src/services/errorService.ts
+++ src/services/errorService.ts
@@ -8,8 +8,9 @@ const isValidationError = (response : AxiosError) => {
 }
 
 const ApplyServerErrorsToRHF = (backendErrors : LaravelErrors, form : UseFormReturn<any>) : void => {
-    
+    console.log("Backend Errors:" , backendErrors)
     Object.entries(backendErrors).forEach(([field, messages]) => {
+        console.log("Dentro de Object Entierers: ", field, messages);
         // Laravel manda array de strings: usamos la primera o las unimos
         const message = messages.join('\n')
 
@@ -20,11 +21,19 @@ const ApplyServerErrorsToRHF = (backendErrors : LaravelErrors, form : UseFormRet
     })
 
 }
+/* TODO: Crear un custom hook parap oder utilziar esta funcion y recibir el MEssage general. */
+export const MergeServerErrorsToForm = (error : AxiosError, form : UseFormReturn<any>) : void => {
 
-export const MergeServerErrorsToForm = (error : AxiosError, form : UseFormReturn) : void => {
+    if(error.response?.data?.message){ // Add standard error message
+        console.log("Agregando error root...")
+        form.setError('root', {
+            type : 'server',
+            message : error.response?.data?.message
+        })
+    }
     
     if(!error.isAxiosError || !isValidationError(error)) return;
     const errors : LaravelErrors = error.response?.data?.error ?? {};
 
-    ApplyServerErrorsToRHF({...errors, message : error.response?.data?.message ?? ''  }, form);
+    ApplyServerErrorsToRHF({...errors }, form);
 }
