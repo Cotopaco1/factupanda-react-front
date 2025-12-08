88b5983e53b4ff1be2fd7f51587d442b81c9c7c4
Sergio <cotopaco123@hotmail.es>
2025-12-08 16:18:47 -0500

    feat: add loading state management to user service methods

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,30 +1,39 @@
 import { apiClient } from "@/lib/apiClient"
 import type { LoginForm } from "@/schemas/login"
 import type { UserType } from "@/types/users"
+import { useState } from "react"
 
 export const useUserService = () => {
+    const [loading, setLoading] = useState(false);
 
     const login = (data : LoginForm) => {
+        setLoading(true);
         return apiClient.post("/stateless/login", data)
         .then((response) => {
             return response.data as {token : string, user : UserType, message : string};
         }).catch((error)=>{
             console.log("Ha habido un error al intentar hacer login ..", error)
             throw error;
-        })
+        }).finally(()=>setLoading(false))
     }
 
     const logout = () => {
+        setLoading(true);
         return apiClient.post("/stateless/logout")
         .then((response)=>{
             return response.data;
-        })
+        }).finally(()=>setLoading(false))
+    }
+      function sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
     }
 
-    const getUser = () => {
+    const getUser = async () => {
+        setLoading(true);
         return apiClient.get('user')
             .then((response)=> response.data as {user : UserType} )
+            .finally(()=>setLoading(false))
     }
 
-    return {login, getUser, logout}
+    return {login, getUser, logout, loading}
 }
