338f7bdfbcd99c96d8738fc3c095b2dd20d09c8f
Sergio <cotopaco123@hotmail.es>
2025-12-13 19:10:55 -0500

    refactor: enhance ButtonLoader props to extend ButtonProps for better flexibility

--- src/components/ButtonLoader.tsx
+++ src/components/ButtonLoader.tsx
@@ -1,13 +1,12 @@
 import type React from "react"
-import { Button } from "./ui/button"
+import { Button, type ButtonProps } from "./ui/button"
 import { Spinner } from "./ui/spinner"
 
-interface Props {
-    loading : boolean,
+interface Props extends Omit<ButtonProps, 'disabled'> {
+    loading : boolean;
     children : React.ReactNode;
-    type ?: "submit" | "button" | "reset" | undefined;
-    className ?: string;
 }
-export function ButtonLoader({loading, children, type='submit', className}:Props){
-    return <Button className={className} type={type} disabled={loading}> {loading && <Spinner/>} {children}</Button>
+
+export function ButtonLoader({loading, children, type='submit', className, ...props}:Props){
+    return <Button className={className} type={type} disabled={loading} {...props}> {loading && <Spinner/>} {children}</Button>
 }
